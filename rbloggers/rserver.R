8d83b8f84dfef1ad39fdcfe7a7b78d40fb323d0b
TinWoodman92 <chrhodgden@gmail.com>
2023-03-05 21:20:27 -0600

    updated message handeling

--- rbloggers/rserver.R
+++ rbloggers/rserver.R
@@ -1,20 +1,30 @@
 HEADER <- 64
+PORT <- 6011
+SERVER <- "localhost"
+FORMAT <- "utf-8"
+DISCONNECT_MESSAGE <- "!DISSCONNECT"
 
-server <- function(){
-  while(TRUE){
-    writeLines("Listening...")
-    con <- socketConnection(
-		host="localhost",
-		port = 6011,
-		blocking=TRUE,
-		server=TRUE,
-		open="r+"
+cat("Listening...\n")
+
+con <- socketConnection(
+	host = "localhost",
+	port = 6011,
+	server = TRUE
 )
+
+connected <- TRUE
+
+while (connected) {
 	data <- trimws(readChar(con, HEADER))
-    print(data)
-    response <- toupper(data)
-	writeChar(response, con)
-    close(con)
-  }
+	while (length(data) == 0) {
+		cat("ALERT\n")
+		data <- trimws(readChar(con, HEADER))
+	}
+	if (data == DISCONNECT_MESSAGE) connected <- FALSE
+	cat(data, "\n")
+	data <- toupper(data)
+	data <- paste(data, strrep(" ", HEADER - nchar(data)), sep = "")
+	writeChar(data, con)
 }
-server()
+
+close(con)
